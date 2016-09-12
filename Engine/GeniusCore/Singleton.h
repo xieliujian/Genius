8a54fbb31a64fe49c6ed86e3996d5f36fb069fed
谢刘建 <377624289@qq.com>
2016-09-12 16:27:38 +0800

    GeniusCore增加Singleton类

---
+++ Engine/GeniusCore/Singleton.h
@@ -0,0 +1,44 @@
+
+
+/*
+Author : Xieliujian
+Data : 2016.9.12
+Description : µ¥¼þÀà
+*/
+
+#ifndef __Singleton_h__
+#define __Singleton_h__
+
+#include "Prerequest.h"
+
+NAMESPACEBEGIN(Genius)
+
+template <class T> class Singleton
+{
+public:
+	Singleton()
+	{
+		mInstance = static_cast<T*>(this);
+	}
+
+	~Singleton()
+	{
+		mInstance = NULL;
+	}
+
+	T* GetInstancePtr()
+	{
+		return mInstance;
+	}
+
+	T& GetInstance()
+	{
+		return *mInstance;
+	}
+protected:
+	static T *mInstance;
+};
+
+NAMESPACEEND
+
+#endif
