7d00ed4ce91bbe780dc9c2b1b5498291a7b5dd92
谢刘建 <377624289@qq.com>
2016-09-12 22:42:22 +0800

    增加Editor库

---
+++ Tools/Editor/GeniusEditor/Editor.cpp
@@ -0,0 +1,3 @@
+
+
+#include "Editor.h"
---
+++ Tools/Editor/GeniusEditor/Editor.h
@@ -0,0 +1,16 @@
+
+
+/*
+Author : Xieliujian
+Data : 2016.9.12
+Description : ±à¼­Æ÷»ùÀà
+*/
+
+#ifndef __GeniusEditor_h__
+#define __GeniusEditor_h__
+
+#include "Prerequest.h"
+
+
+
+#endif
