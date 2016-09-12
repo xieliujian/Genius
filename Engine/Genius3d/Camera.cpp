70685b38a6cd4b5684fa9284ab1951c65c7f3904
谢刘建 <377624289@qq.com>
2016-09-12 16:18:00 +0800

    Genius3d增加Camera类

---
+++ Engine/Genius3d/Camera.cpp
@@ -0,0 +1,17 @@
+
+
+#include "Camera.h"
+
+NAMESPACEBEGIN(Genius)
+
+Camera::Camera()
+{
+
+}
+
+Camera::~Camera()
+{
+
+}
+
+NAMESPACEEND
---
+++ Engine/Genius3d/Camera.h
@@ -0,0 +1,25 @@
+
+
+/*
+Author : Xieliujian
+Data : 2016.9.12
+Description : …„œÒª˙¿‡
+*/
+
+#ifndef __Camera_h__
+#define __Camera_h__
+
+#include "Prerequest.h"
+
+NAMESPACEBEGIN(Genius)
+
+class Camera
+{
+public:
+	Camera();
+	~Camera();
+};
+
+NAMESPACEEND
+
+#endif
